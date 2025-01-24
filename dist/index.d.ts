3a4bbc5a0eaab2ce96812291386c1075d9c07391
saradab-mindfire <saradab@mindfiresolutions.com>
2025-01-24 16:18:50 +0530

    fix: Resolved merge conflicts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,10 @@
-export interface CurrentAttributeDTO {
+interface CurrentAttributeDTO {
     bold: boolean;
     italic: boolean;
     underline: boolean;
     undo?: boolean;
     redo?: boolean;
+    hyperlink?: string | boolean;
 }
+
+export type { CurrentAttributeDTO };
