34a1a6c8b6c40f54c02dbb775e539520fbdd7ec8
saradab-mindfire <saradab@mindfiresolutions.com>
2025-04-28 10:54:23 +0530

    fix(angular): .angular ignored for linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,37 @@
-import tsParser from "@typescript-eslint/parser";
-import globals from "globals";
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
 
 export default [
   {
     ignores: [
-      "**/node_modules/**",
-      "**/test/**",
-      "**/dist/**",
-      "**/*.d.ts",
+      '**/node_modules/**',
+      '**/test/**',
+      '**/dist/**',
+      '**/*.d.ts',
+      '**/.angular/**',
     ],
   },
   {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
         ecmaVersion: 2022,
-        sourceType: "module",
+        sourceType: 'module',
       },
       globals: {
         ...globals.builtin,
       },
     },
     rules: {
-      "no-unused-vars": [
-        "error",
+      'no-unused-vars': [
+        'error',
         {
-          vars: "all",
-          args: "none",
+          vars: 'all',
+          args: 'none',
           ignoreRestSiblings: false,
         },
       ],
-      
     },
   },
 ];
