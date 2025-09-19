4f0efcd2989b0fcc19753123c13f04e85a89b3f1
Parth <parthd@mindfiresolutions.com>
2025-09-19 06:42:59 +0000

    remove dist folder

--- dist/view/toolbarView.d.ts
+++
@@ -1,9 +0,0 @@
-import { CurrentAttributeDTO } from "..";
-import EventEmitter from "../utils/events";
-declare class ToolbarView extends EventEmitter {
-    container: HTMLElement;
-    constructor(container: HTMLElement);
-    setupButtons(): void;
-    updateActiveStates(attributes: CurrentAttributeDTO): void;
-}
-export default ToolbarView;
