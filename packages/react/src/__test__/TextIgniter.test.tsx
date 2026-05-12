12073a9d33179f795ded1fbb4b937aaa028157ee
sobhan-s <sobhans@mindfiresolutions.com>
2026-05-12 18:46:27 +0530

    fix(react): fix the import of test cases of react testcases

--- packages/react/src/__test__/TextIgniter.test.tsx
+++ packages/react/src/__test__/TextIgniter.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { TextigniterReact } from '../components/TextIgniter';
 
 // Mock the dynamic import for the web component
