fa6b2d83f5aa0f2ac90027e26a9cbce2c0ca4197
sobhan-s <sobhans@mindfiresolutions.com>
2026-05-12 15:10:21 +0530

    fix(web-component): fix the web-component build issue

--- packages/web-component/tsup.config.ts
+++ packages/web-component/tsup.config.ts
@@ -4,15 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   noExternal: ['@mindfiredigital/textigniter'],
-  dts: {
-    compilerOptions: {
-      composite: true,
-      paths: {
-        '*': ['*'],
-      },
-      lib: ['DOM', 'DOM.Iterable', 'ESNext'],
-    },
-  },
+  dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
